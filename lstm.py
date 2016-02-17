404c419f455c94af4820eb96e89c61cc91f93a99
Nathan Kjer <nmkjer+git@gmail.com>
2016-02-17 03:16:21 -0500

    Pilot commit

---
+++ lstm.py
@@ -0,0 +1,108 @@
+import time
+
+from keras.models import Sequential, model_from_json
+from keras.layers import containers
+from keras.layers.core import TimeDistributedDense, Activation, Dropout, AutoEncoder
+from keras.layers.recurrent import GRU, LSTM
+from keras.callbacks import EarlyStopping
+
+import numpy as np
+np.random.seed(1)
+
+import matplotlib.pyplot as plt
+
+import theano
+theano.config.mode = 'FAST_RUN'
+theano.config.floatX = 'float32'
+
+#
+# Model I/O
+#
+
+# Save model
+def saveModel(model):
+	t = int(time.time())
+	open('models/%s_meta.json' % t, 'w').write(model.to_json())
+	model.save_weights('models/%s_data.h5' % t, overwrite=True)
+	return None
+
+# Load model
+def loadModel(name):
+	model = model_from_json(open('models/%s_meta.json' % name).read())
+	model.load_weights('models/%s_data.h5' % name)
+	return model
+
+# Use model
+def runModel(inMatrix, model):
+	return model.predict(inMatrix)
+
+
+def trainEncoder(inMatrix, outMatrix):
+
+	print("Compiling Encoder...")
+	inSize = inMatrix.shape[2]
+	encoder = containers.Sequential([LSTM(output_dim=128, input_dim = 256, return_sequences=True), \
+                                     LSTM(output_dim=64, input_dim = 128, return_sequences=True), \, \
+                                     LSTM(output_dim=32, input_dim = 64, return_sequences=True)
+                                     LSTM(output_dim=8, input_dim = 32, return_sequences=True), \
+                                     LSTM(output_dim=2, input_dim = 8, return_sequences=True)])
+	decoder = containers.Sequential([LSTM(output_dim=8, input_dim = 2, return_sequences=True), \
+	                                 LSTM(output_dim=32, input_dim = 8, return_sequences=True), \
+	                                 LSTM(output_dim=64, input_dim = 32, return_sequences=True), \
+                                     LSTM(output_dim=128, input_dim = 64, return_sequences=True), \
+                                     LSTM(output_dim=256, input_dim = 128, return_sequences=True)])
+									 
+	autoencoder = AutoEncoder(encoder=encoder, decoder=decoder, output_reconstruction=True)
+	
+	model = Sequential()
+	model.add(autoencoder)
+	model.compile(loss='mse', optimizer='rmsprop')
+	
+	# Train model
+	print("Training Encoder...")
+	early_stopping = EarlyStopping(monitor='val_loss', patience=1)
+	model.fit(inMatrix, inMatrix, batch_size=inMatrix.shape[0]/20, validation_split=0.15, callbacks=[early_stopping], verbose=1)
+
+	autoencoder.output_reconstruction = False
+	return model
+
+#
+# Train model
+#
+
+def trainModel(inMatrix, targMatrix):
+	assert inMatrix.shape[:-1] == targMatrix.shape[:-1]
+	
+	autoencoder = trainEncoder(inMatrix, outMatrix)
+	#inMatrix = autoencoder.predict(inMatrix)
+	
+	'''
+	# Compile model
+	print("Compiling Model...")
+	inSize, outSize = inMatrix.shape[2], targMatrix.shape[2]
+	
+	model = Sequential()
+	model.add(LSTM(output_dim=inSize//1.25, input_dim=inSize, return_sequences=True))
+	model.add(Dropout(0.8))
+	model.add(LSTM(output_dim=inSize//2.5, input_dim=inSize//1.25, return_sequences=True))
+	model.add(Dropout(0.4))
+	model.add(LSTM(output_dim=inSize/3.75, input_dim=inSize//2.5, return_sequences=True))
+	model.add(Dropout(0.2))
+	model.add(LSTM(output_dim=inSize/5, input_dim=inSize//3.75, return_sequences=True))
+	model.add(Dropout(0.1))
+	model.add(LSTM(output_dim=outSize, input_dim=inSize//5, return_sequences=True))
+	model.add(TimeDistributedDense(outSize))
+	model.add(Activation("hard_sigmoid"))
+	
+	model.compile(loss="mse", optimizer="rmsprop")
+	
+	# Train model
+	print("Training Model...")
+	early_stopping = EarlyStopping(monitor='val_loss', patience=1)
+	model.fit(inMatrix, targMatrix, batch_size=inMatrix.shape[0]/20, validation_split=0.15, callbacks=[early_stopping], verbose=1)
+	
+	# Save model
+	print("Saving Model...")
+	saveModel(model)'''
+
+	return model
---
+++ main.py
@@ -0,0 +1,78 @@
+import csv, random
+import numpy as np
+
+import lstm
+
+# File I/O
+def loadfile(fname):
+	bits = 8
+	try:
+		fmat = np.fromfile(fname, dtype=np.dtype('uint%s' % bits))
+		result = np.zeros((fmat.size, 2**bits))
+		result[np.arange(fmat.size), fmat] = 1
+		return np.expand_dims(result, axis=0)
+	except IOError:
+		return np.empty((0,0,0))
+	
+# Data preparation
+def sample(mat, w=100, n=5):
+	assert mat.shape[1] > w
+	result = []
+	for x in xrange(n):
+		i = random.randint(0, mat.shape[1]-w)
+		result.append(mat[:, i:i+w])
+	return np.concatenate(result, axis=0)
+
+def getfiles():
+	with open('classes.csv', 'rb') as csvfile:
+		flist = list(csv.reader(csvfile, delimiter=',', quotechar='"'))[1:]
+	random.shuffle(flist)
+	return [(x[0], np.array(x[1:], dtype=float)) for x in flist]
+
+def readfiles():
+
+	fsamps = 5			# Samples per file
+	nsamps = 100		# Samples in total
+	window = 160
+	
+	inMatrix, targMatrix = [], []				# Extract training data
+	for f in getfiles()[:nsamps//fsamps]:
+		print f[0]
+		fmat = loadfile(f[0])					# Load file
+		if fmat.shape[1] > window:
+			fmat = sample(fmat, window, fsamps)	# Sample
+			inMatrix.append(fmat)
+			
+			tmat = np.tile(f[1], (fmat.shape[0], fmat.shape[1], 1))
+			targMatrix.append(tmat)
+	
+	# Turn lists into matrices (online concatenation is slow)
+	inMatrix = np.concatenate(inMatrix, axis=0)
+	targMatrix = np.concatenate(targMatrix, axis=0)
+	
+	# Shuffle matrices
+	p = np.random.permutation(inMatrix.shape[0])
+	return (inMatrix[p], targMatrix[p])
+
+#Main
+def main():
+
+	# Get model, either through training or loading
+	if True:
+		inMatrix, targMatrix = readfiles()
+		model = lstm.trainModel(inMatrix, targMatrix)
+	else:
+		model = lstm.loadModel('')
+		
+	# Backtest model
+	for f in getfiles():
+		print 'Opening ' + f[0] + '...'
+		fmat = loadfile(f[0])
+		result = lstm.runModel(fmat, model)
+		print np.mean(result, axis=1)
+		print np.max(result, axis=1)
+		print np.min(result, axis=1)
+		print
+
+if __name__ == "__main__":
+	main()
