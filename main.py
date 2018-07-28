b29d794d922663497c9a90a3714d8de6eadc2cd4
Ralph Rajko-Nenow <internety@users.noreply.github.com>
2018-07-28 05:53:24 +0100

    variable name change to avoid using reserved keyword "input"

--- main.py
+++ main.py
@@ -19,16 +19,16 @@ def main():
 	if next(os.walk('data'))[1]:
 		retrain = False
 		if retrain:
-			input, target, classes = data.sample('data')
-			model = modeler.build(input.shape, target.shape)
-			modeler.train(model, input, target)
+			input_, target, classes = data.sample('data')
+			model = modeler.build(input_.shape, target.shape)
+			modeler.train(model, input_, target)
 			modeler.save(model, classes)
 		else:
 			model, classes = modeler.load(sorted(os.listdir('models'))[-1])
 			with open("data/harry-potter/Sorcerer's Stone.txt") as f:
-				input = data.str2mat(f.read())
-			output = modeler.run(model, input)
-			data.backtest(classes, input, output)
+				input_ = data.str2mat(f.read())
+			output = modeler.run(model, input_)
+			data.backtest(classes, input_, output)
 	else:
 		print("""\nNo data found.\nPut subfolders of files by class, within the 'data' folder.""")
 
